73954ea99bc9f696d1c22cdbd2a3a883021cd467
L200170047 <43442320+L200170047@users.noreply.github.com>
2019-03-05 20:54:41 +0700

    Add files via upload

---
+++ modul1/2.py
@@ -0,0 +1,7 @@
+def gambarlahPersegiEmpat(p, l):
+    for i in range(1, p+1):
+        if i==1 or i==p:
+            print("@"*l)
+        else:
+            print("@"+" "*(l-2)+"@")
+gambarlahPersegiEmpat(4, 5)
---
+++ modul1/6.py
@@ -0,0 +1,2 @@
+for i in range(2, 1001):
+    print(str(i)+" "+str(apakahPrima(i)))
