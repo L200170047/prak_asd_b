544b5e53001afc12589826fb34fb6f78c26a7b43
L200170047 <43442320+L200170047@users.noreply.github.com>
2019-03-25 22:53:41 +0700

    Add files via upload

---
+++ modul4/7.py
@@ -0,0 +1,25 @@
+def binSeMass(kumpulan, target):
+    temp = []
+    low = 0
+    high = len(kumpulan)-1
+    while low <= high :
+        mid = (high+low)//2
+        if kumpulan[mid] == target:
+            midKiri = mid-1
+            while kumpulan[midKiri] == target:
+                temp.append(midKiri)
+                midKiri = midKiri-1
+            temp.append(mid)
+            midKanan = mid+1
+            while kumpulan[midKanan] == target:
+                temp.append(midKanan)
+                midKanan = midKanan+1
+            return temp
+        elif target < kumpulan[mid]:
+            high = mid-1
+        else:
+            low = mid+1
+    return False
+
+kumpulan = [2, 3, 3, 5, 5, 5, 5, 9, 12, 15, 17, 21, 31]
+print(binSeMass(kumpulan, 6))
